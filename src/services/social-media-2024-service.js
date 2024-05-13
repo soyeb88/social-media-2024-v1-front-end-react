dca48b0344566d7660217ee47e7a2c7546b9c36d
Soyeb Ahmed <soyeb88@gmail.com>
2024-05-13 12:03:09 -0400

    added axios

---
+++ src/services/social-media-2024-service.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const ACCOUNT_API_BASE_URL="http://localhost:8082/api/v1/employees";
+
+class SocialMediaService{
+    createAccount(){
+        return axios.post();
+    }
+}
+
+export default new SocialMediaService();
